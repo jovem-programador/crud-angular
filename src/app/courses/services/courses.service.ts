305ebeaa86785dd72f55e1751d8576761c2ccca6
Anderson Marley <andersonmarley011@gmail.com>
2023-09-09 22:22:36 -0300

    Metodo list criado

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { Course } from '../model/course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  // Inicializando o serviço HttpClient
+  constructor(private httpClient: HttpClient) { }
+
+  // Metodo do tipo Course responsavel por retornar os cursos
+  list(): Course[] {
+    return [
+      {
+        _id: '1',
+        name: 'Angular',
+        category: 'Front-End'
+      }
+    ];
+  }
 }
