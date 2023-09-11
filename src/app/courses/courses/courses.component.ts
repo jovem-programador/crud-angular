4811a0b9d2e103ed1764fba77e54a41f9815d21a
Anderson Marley <andersonmarley011@gmail.com>
2023-09-10 22:45:10 -0300

    refatoração de variavel

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -6,20 +6,18 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-
   // Inicialização *courses -> dataSource | do tipo Course* | tratamento de dados da lista
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
 
   // Colunas da lista
   displayedColumns = ['name', 'category'];
 
   // Inicializando o *service*
   constructor(private coursesService: CoursesService) {
-
     // chamando o  metodo e passando para variavel responsavel pelo dataSource
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
   }
 }
