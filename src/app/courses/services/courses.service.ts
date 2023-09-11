6a302ee3dd3b7c3018558ea13248eb6e4f0377c0
Anderson Marley <andersonmarley011@gmail.com>
2023-09-10 22:26:36 -0300

    Mudanças na list()

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,23 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Course } from '../model/course';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = "../../../assets/courses.json";
+
   // Inicializando o serviço HttpClient
   constructor(private httpClient: HttpClient) { }
 
   // Metodo do tipo Course responsavel por retornar os cursos
-  list(): Course[] {
-    return [
-      {
-        _id: '1',
-        name: 'Angular',
-        category: 'Front-End'
-      }
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      tap(courses => console.log(courses))
+    );
   }
 }
