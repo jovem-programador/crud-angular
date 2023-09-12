9f11f78cc54e433bb80e3759b33817df1928488e
Anderson Marley <andersonmarley011@gmail.com>
2023-09-11 22:35:17 -0300

    Adicionado o modulo de importação

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    AppMaterialModule
+    AppMaterialModule,
+    SharedModule
   ]
 })
 export class CoursesModule { }
