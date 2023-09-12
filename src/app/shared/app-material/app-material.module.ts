736f549254156d7b3eda1581c1a1f9dcc39fb039
Anderson Marley <andersonmarley011@gmail.com>
2023-09-11 22:37:22 -0300

    adicionado os butões

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   exports: [
@@ -10,6 +12,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatTableModule,
     MatToolbarModule,
     MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule
   ],
 })
 export class AppMaterialModule {}
