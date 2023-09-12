bd0ebeec3d16a646abef4ad610c61ada318277ab
Anderson Marley <andersonmarley011@gmail.com>
2023-09-11 22:36:31 -0300

    Adicionado o popup

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -19,8 +21,24 @@ export class CoursesComponent {
   displayedColumns = ['name', 'category'];
 
   // Inicializando o *service*
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
+
     // chamando o  metodo e passando para variavel responsavel pelo dataSource
-    this.courses$ = this.coursesService.list();
+    this.courses$ = this.coursesService.list().pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos.')
+        // of([]) retorna um observablo vazio, para parar o spinner
+        return of([])
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 }
