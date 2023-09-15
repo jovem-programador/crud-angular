a671d06694032ba8b08d4673774222d106447fdb
Anderson Marley <andersonmarley011@gmail.com>
2023-09-14 21:45:41 -0300

    Rota do formulario adicionada

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CoursesComponent } from './courses/courses.component';
+import { CourseFormComponent } from './course-form/course-form.component';
 
 const routes: Routes = [
   {
     // Rota vazia, chama o "courses component"
     path: '', component: CoursesComponent
+  },
+  {
+    // Rota do formulario "Adicionar curso"
+    path: 'new', component: CourseFormComponent
   }
 ];
 
