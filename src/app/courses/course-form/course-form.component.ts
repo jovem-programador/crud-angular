614c1c266c76e77d713a980f6775c5544c89411c
Anderson Marley <andersonmarley011@gmail.com>
2023-09-18 17:12:55 -0300

    Logica do formulario

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class CourseFormComponent {
 
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar) {
+
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null]
+    });
+  }
+
+  onSubmit() {
+    this.service.save(this.form.value).subscribe(result => console.log(result),
+     error => {
+      this.onError();
+      console.log(error);
+    });
+  }
+
+  onCancel() {
+    console.log("Cancelar");
+  }
+
+  private onError() {
+    this.snackBar.open('Error ao salvar curso.', '', { duration: 5000 });
+  }
 }
