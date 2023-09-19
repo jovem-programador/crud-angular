b7af5a28db2ee83e705105d895558478c5d9c6ca
Anderson Marley <andersonmarley011@gmail.com>
2023-09-18 22:31:56 -0300

    Adiciona o metodo onSucess() -Mensagem de sucesso

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { CoursesService } from '../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
@@ -15,7 +16,8 @@ export class CourseFormComponent {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar) {
+    private snackBar: MatSnackBar,
+    private location: Location) {
 
     this.form = this.formBuilder.group({
       name: [null],
@@ -24,18 +26,22 @@ export class CourseFormComponent {
   }
 
   onSubmit() {
-    this.service.save(this.form.value).subscribe(result => console.log(result),
+    this.service.save(this.form.value).subscribe(result => this.onSucess(),
      error => {
       this.onError();
-      console.log(error);
     });
   }
 
   onCancel() {
-    console.log("Cancelar");
+    this.location.back();
   }
 
-  private onError() {
+  onSucess() {
+    this.snackBar.open('Curso salvo com sucesso!!', '', { duration: 5000 });
+    this.onCancel();
+  }
+
+  onError() {
     this.snackBar.open('Error ao salvar curso.', '', { duration: 5000 });
   }
 }
