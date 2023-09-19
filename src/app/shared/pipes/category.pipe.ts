9a583db4826935c80f800f73cc7e7ea5b47d5076
Anderson Marley <andersonmarley011@gmail.com>
2023-09-18 21:26:27 -0300

    Pipe para alteração de icones

--- src/app/shared/pipes/category.pipe.ts
+++ src/app/shared/pipes/category.pipe.ts
@@ -6,11 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CategoryPipe implements PipeTransform {
 
   transform(value: string): string {
-    switch(value) {
+    switch(value.toLowerCase()) {
       case 'front-end': return 'code';
       case 'back-end': return 'storage';
     }
-    return 'code';
+    return 'null';
   }
 
 }
