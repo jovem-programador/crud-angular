35453e1b7c0c5c4bbcd5da21cfdffe00861bd363
Anderson Marley <andersonmarley011@gmail.com>
2023-09-27 20:20:25 -0300

    Adicionado metodos

--- src/app/courses/courses-list/courses-list.component.ts
+++ src/app/courses/courses-list/courses-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
-  styleUrls: ['./courses-list.component.scss']
+  styleUrls: ['./courses-list.component.scss'],
 })
 export class CoursesListComponent {
+  @Input() courses: Course[] = [];
 
+  // Colunas da lista
+  readonly displayedColumns = ['name', 'category', 'actions'];
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  onAdd() {
+    // Navega para a rota "new", relativa a rota atual
+    this.router.navigate(['new'], { relativeTo: this.route });
+  }
 }
