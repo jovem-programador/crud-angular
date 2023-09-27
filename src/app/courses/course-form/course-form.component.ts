83498f2edc565eb2dfb2886bb331f5a70e534365
Anderson Marley <andersonmarley011@gmail.com>
2023-09-27 18:38:55 -0300

    Validação basica de formulario

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { CoursesService } from '../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Location } from '@angular/common';
@@ -11,19 +11,17 @@ import { Location } from '@angular/common';
 })
 export class CourseFormComponent {
 
-  form: FormGroup;
-
   constructor(
-    private formBuilder: FormBuilder,
+    private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
     private location: Location) {
-
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null]
-    });
   }
+
+  form = this.formBuilder.group({
+    name: [''],
+    category: ['']
+  });
 
   onSubmit() {
     this.service.save(this.form.value).subscribe(result => this.onSucess(),
