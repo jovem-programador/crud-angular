54e0a0993b6c9393498f498e85b0b05df7260a0d
Anderson Marley <andersonmarley011@gmail.com>
2023-09-30 21:42:19 -0300

    Setando dados do curso, atraves do id no form

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -3,6 +3,8 @@ import { NonNullableFormBuilder } from '@angular/forms';
 import { CoursesService } from '../../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../../model/course';
 
 @Component({
   selector: 'app-course-form',
@@ -15,14 +17,28 @@ export class CourseFormComponent {
     private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
-    private location: Location) {
+    private location: Location,
+    private route: ActivatedRoute) {
+
+      // Pegando dados da rota EDIT
+      const course: Course = this.route.snapshot.data['course'];
+
+      // Setando dados no formulario
+      this.form.setValue({
+        _id: course._id,
+        name: course.name,
+        category: course.category
+      })
   }
 
+  // Inicialização do formulario
   form = this.formBuilder.group({
+    _id: [''],
     name: [''],
     category: ['']
   });
 
+  // Cadastrando curso
   onSubmit() {
     this.service.save(this.form.value).subscribe(result => this.onSucess(),
      error => {
@@ -30,15 +46,18 @@ export class CourseFormComponent {
     });
   }
 
+  // Botão de cancelar
   onCancel() {
     this.location.back();
   }
 
+  // Mensagem de sucesso
   onSucess() {
     this.snackBar.open('Curso salvo com sucesso!!', '', { duration: 5000 });
     this.onCancel();
   }
 
+  // Mensagem de error
   onError() {
     this.snackBar.open('Error ao salvar curso.', '', { duration: 5000 });
   }
