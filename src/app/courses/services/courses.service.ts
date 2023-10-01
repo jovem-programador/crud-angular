7067db15d8ba7565341f57a28c7195cc6c9a29a6
Anderson Marley <andersonmarley011@gmail.com>
2023-09-30 21:42:55 -0300

    Metodo PUT adicionado | refatoração de codigo

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -13,19 +13,35 @@ export class CoursesService {
   constructor(private httpClient: HttpClient) {}
 
   list() {
-    return this.httpClient
-      .get<Course[]>(this.API).pipe(
-        delay(1000),
-        first(),
-        tap((courses) => console.log(courses))
-      );
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      delay(1000),
+      first(),
+      tap((courses) => console.log(courses))
+    );
   }
 
-  save(record: Partial<Course>) {
-   return this.httpClient.post<Course>(this.API, record);
-  }
-
+  // Pegando os dados do curso atraves do ID
   loadById(id: string) {
     return this.httpClient.get<Course>(`${this.API}/${id}`);
+  }
+
+  // Verifica se o ID existe, chama o POST ou PUT
+  save(record: Partial<Course>) {
+
+    if (record._id) {
+      return this.update(record);
+    }
+
+    return this.create(record);
+  }
+
+  // POST do banco de dados
+  private create(record: Partial<Course>) {
+    return this.httpClient.post<Course>(this.API, record);
+  }
+
+  // PUT do banco de dados
+  private update(record: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record);
   }
 }
