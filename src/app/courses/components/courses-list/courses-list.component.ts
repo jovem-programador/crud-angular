282d39d29d05d4eabf777e2457bd4e2107401a61
Anderson Marley <andersonmarley011@gmail.com>
2023-10-03 17:11:03 -0300

    Tirando imports desnecessarios

--- src/app/courses/components/courses-list/courses-list.component.ts
+++ src/app/courses/components/courses-list/courses-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { Course } from '../../model/course';
 
 @Component({
