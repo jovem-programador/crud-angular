3979f238350527625d3d377a7506b13be59cc90d
Anderson Marley <andersonmarley011@gmail.com>
2023-10-03 17:09:42 -0300

    Component responsavel pelo dialog

---
+++ src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.css']
+})
+export class ConfirmationDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: string,
+  ) {}
+
+  ngOnInit() {
+  }
+
+  onComfirm(result: boolean): void {
+    this.dialogRef.close(result);
+  }
+
+}
