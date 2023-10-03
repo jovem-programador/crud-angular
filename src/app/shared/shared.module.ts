e661c6cd3e823db44d3e5dfb6ed4d1c8148c7d7e
Anderson Marley <andersonmarley011@gmail.com>
2023-10-03 17:10:03 -0300

    Importação do dialog component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { AppMaterialModule } from './app-material/app-material.module';
 import { CategoryPipe } from './pipes/category.pipe';
+import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
 
 @NgModule({
-  declarations: [ErrorDialogComponent, CategoryPipe],
+  declarations: [ErrorDialogComponent, CategoryPipe, ConfirmationDialogComponent],
   imports: [CommonModule, AppMaterialModule],
-  exports: [ErrorDialogComponent, CategoryPipe],
+  exports: [ErrorDialogComponent, CategoryPipe, ConfirmationDialogComponent],
 })
 export class SharedModule {}
