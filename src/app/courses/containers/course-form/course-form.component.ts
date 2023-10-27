778dd9c22feef7d4d1b6777c7e8f2f6d26efd960
Anderson Marley <andersonmarley011@gmail.com>
2023-10-27 14:15:18 -0300

    Formulario para criação de aulas

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  NonNullableFormBuilder,
+  UntypedFormArray,
+  Validators,
+} from '@angular/forms';
 import { CoursesService } from '../../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from '../../model/course';
+import { Lesson } from '../../model/lesson';
 
 @Component({
   selector: 'app-course-form',
@@ -12,6 +18,10 @@ import { Course } from '../../model/course';
   styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent {
+  //Inicialização do formulario
+  form!: FormGroup;
+
+  // Construtor
   constructor(
     private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
@@ -21,30 +31,88 @@ export class CourseFormComponent {
   ) {
     // Pegando dados da rota EDIT
     const course: Course = this.route.snapshot.data['course'];
-
     // Setando dados no formulario
-    this.form.setValue({
-      _id: course._id,
-      name: course.name,
-      category: course.category,
+    this.form = this.formBuilder.group({
+      _id: [course._id],
+      name: [
+        course.name,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(250),
+        ],
+      ],
+      category: [course.category, [Validators.required]],
+      lessons: this.formBuilder.array(
+        this.retrieveLessons(course),
+        Validators.required
+      ),
     });
   }
 
-  // Inicialização do formulario
-  form = this.formBuilder.group({
-    _id: [''],
-    name: ['',[Validators.required, Validators.minLength(5), Validators.maxLength(250)]],
-    category: ['', [Validators.required]],
-  });
+  private retrieveLessons(course: Course) {
+    // Atribuindo um Array vazio
+    const lessons = [];
+
+    // Caso ' lessons ' exista
+    if (course?.lessons) {
+      // Percore os dados e faz um push das Aulas
+      course.lessons.forEach((lesson) =>
+        lessons.push(this.createLesson(lesson))
+      );
+    } else {
+      // Caso não exista, cria a Aula vazia
+      lessons.push(this.createLesson());
+    }
+
+    // Retorna as Aulas
+    return lessons;
+  }
+
+  // Lesson do Curso
+  private createLesson(lesson: Lesson = { id: '', name: '', youtubeUrl: '' }) {
+    return this.formBuilder.group({
+      id: [lesson.id],
+      name: [lesson.name,
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(100)],
+      youtubeUrl: [lesson.youtubeUrl,
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(11)],
+    });
+  }
+
+  // Pegando dados do Array
+  getLessonsFormArray() {
+    return (<UntypedFormArray>this.form.get('lessons')).controls;
+  }
+
+  // Adicionando uma nova Aula
+  addNewLesson() {
+    const lessons = this.form.get('lessons') as UntypedFormArray;
+    lessons.push(this.createLesson());
+  }
+
+  // Removando Aula
+  removeLesson(index: number) {
+    const lessons = this.form.get('lessons') as UntypedFormArray;
+    lessons.removeAt(index);
+  }
 
   // Cadastrando curso
   onSubmit() {
-    this.service.save(this.form.value).subscribe(
-      (result) => this.onSucess(),
-      (error) => {
-        this.onError();
-      }
-    );
+    if (this.form.valid) {
+      this.service.save(this.form.value).subscribe(
+        (result) => this.onSucess(),
+        (error) => {
+          this.onError();
+        }
+      );
+    } else {
+      alert('Form invalido');
+    }
   }
 
   // Botão de cancelar
@@ -67,25 +135,32 @@ export class CourseFormComponent {
   getErrorMessage(fieldName: string) {
     const field = this.form.get(fieldName);
 
-    if(field?.hasError('required')) {
-
+    if (field?.hasError('required')) {
       return 'Campo obrigatorio';
     }
 
-    if(field?.hasError('minlength')) {
-
-      const requiredLength = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+    if (field?.hasError('minlength')) {
+      const requiredLength = field.errors
+        ? field.errors['minlength']['requiredLength']
+        : 5;
 
       return `Tamanho minimo precisa ser de ${requiredLength} caracteres`;
     }
 
-    if(field?.hasError('maxlength')) {
-
-      const requiredLength = field.errors ? field.errors['maxlength']['requiredLength'] : 250;
+    if (field?.hasError('maxlength')) {
+      const requiredLength = field.errors
+        ? field.errors['maxlength']['requiredLength']
+        : 250;
 
       return `Tamanho máximo exedido de ${requiredLength} caracteres`;
     }
 
     return 'Campo invalido';
+  }
+
+  isFormArrayRequired() {
+    const lessons = this.form.get('lessons') as UntypedFormArray;
+    return !lessons.valid && lessons.hasError('required') && lessons.touched;
+
   }
 }
